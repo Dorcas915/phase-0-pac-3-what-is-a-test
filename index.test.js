86f73a761fca2cb93d38e6216f1cd42fef92d188
Dorcas Chepkoech <dorcaschepkoech717@gmail.com>
2025-08-28 14:05:36 +0300

    All tests passing

---
+++ index.test.js
@@ -0,0 +1,20 @@
+const { name, height, message } = require("./index");
+
+describe("Name", () => {
+  it('returns "Susan"', () => {
+    expect(name).toEqual("Susan");
+  });
+});
+
+describe("Height", () => {
+  it("is less than 40", () => {
+    expect(height).toBeLessThan(40);
+  });
+});
+
+describe("Message", () => {
+  it("gives the name and height", () => {
+    expect(message).toContain(name);
+    expect(message).toContain(height.toString());
+  });
+});
